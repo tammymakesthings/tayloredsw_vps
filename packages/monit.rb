d05412470f9e0c5bb2b0419faa74a6bd3068877d
Tammy Cravit <tcravit@taylored-software.com>
2011-12-08 10:10:49 -0800

    Add recipe for monit

---
+++ packages/monit.rb
@@ -0,0 +1,34 @@
+MONIT_CONFIG_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..', 'assets', 'monit', 'configuration'))
+MONIT_SCRIPT_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..', 'assets', 'monit', 'script'))
+
+package :monit_tools do 
+  description "Dependencies for monit"
+  apt %w(flex bison)
+  
+  verify do
+    has_executable 'flex'
+    has_executable 'bison'
+  end
+end
+
+package :monit do  
+  requires :monit_tools
+  
+  describe 'Monitor processes, files, devices and remote systems'
+  apt 'monit' do
+    if File.file?(MONIT_SCRIPT_PATH) && File.file?(MONIT_CONFIG_PATH)
+      config = File.open(MONIT_CONFIG_PATH).read.gsub("'", "'\\\\''").gsub("\n", '\n')
+      script = File.open(MONIT_SCRIPT_PATH).read.gsub("'", "'\\\\''").gsub("\n", '\n')
+
+      post :install,
+        "echo -e '#{script}' > /etc/default/monit",
+        "echo -e '#{config}' >/etc/monit/monitrc"
+        "monit"
+    end
+  end
+
+  verify do
+    has_executable 'monit'
+  end
+end
+
